e1566a1e1b283a016427cc2f5323fcdcd0f684ee
Muhammad <mohammedsoban1@gmail.com>
2018-12-22 11:30:00 +0500

    insert data into database

---
+++ WAD projects/server.js
@@ -0,0 +1,43 @@
+
+var mysql = require('mysql');
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const cors = require('cors')
+app.use(bodyParser.json());
+app.use(cors())
+const port = 3000
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'event management',
+    port: '3306'
+});
+
+connection.connect(function (err) {
+    if (!err) {
+        console.log("Database is connected");
+    } else {
+        console.log("Error connecting database", err);
+    }
+});
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/example', (req, res) => {
+    let { firstname, lastname, email, date, phone, password } = req.body
+
+    var sql = `INSERT INTO signup (f_name, l_name, email, date, phone, password) VALUES ('${firstname}', '${lastname}', '${email}' , '${date}', '${phone}', '${password}')`;
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.log(err)
+            throw err;
+        };
+        console.log("1 record inserted");
+    });
+    res.send(req.body)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
