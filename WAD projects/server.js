312cb4a7c0f05e25767a7f8c828e0119aaf9c819
MohammedSoban <mohammedsoban1@gmail.com>
2018-12-25 04:23:21 +0500

    vendor signup form created and connected to DB

--- WAD projects/server.js
+++ WAD projects/server.js
@@ -26,18 +26,47 @@ connection.connect(function (err) {
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.post('/example', (req, res) => {
-    let { firstname, lastname, email, dob, phone, password } = req.body
+app.post('/userSigupForm', (req, res) => {
+    let { firstname, lastname, email, dob, address, phone, password } = req.body
  
-    var sql = `INSERT INTO signup (f_name, l_name, dob, email_id, phone_num, password) VALUES ('${firstname}', '${lastname}','${dob}','${email}', '${phone}', '${password}')`;
+    var sql = `INSERT INTO signup (f_name, l_name, dob, Address, email_id, phone_num, password) VALUES ('${firstname}', '${lastname}','${dob}','${address}','${email}', '${phone}', '${password}')`;
     connection.query(sql, (err, result) => {
         if (err) {
             console.log(err)
-            throw err;
+            
+        throw err;
+           
         };
+        
+        
         console.log("1 record inserted");
     });
     res.send(req.body)
 })
+
+app.post('/vendorSigupForm', (req, res) => {
+    let { firstname,
+        lastname,
+        dob,
+        email,
+        companyName,
+        companyAddress,
+        phone,
+        password } = req.body
+ 
+    var sql = `INSERT INTO vendorsignup (f_name, l_name, dob, email_id, company_name, company_address, phone, password) VALUES ('${firstname}', '${lastname}','${dob}','${email}','${companyName}','${companyAddress}','${phone}', '${password}')`;
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.log(err)
+            
+        throw err;
+           
+        };    
+        console.log("1 record inserted");
+    });
+    res.send(req.body)
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
