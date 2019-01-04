c537bf7c12e574b86318861e1b9ec9720c6e41c4
Muhammad <mohammedsoban1@gmail.com>
2019-01-04 21:11:55 +0500

    login fixed

--- WAD projects/server.js
+++ WAD projects/server.js
@@ -66,37 +66,29 @@ app.post('/vendorSigupForm', (req, res) => {
 
 app.post('/login', (req, res) => {
     let {
-        email_id, 
+        email_id,
         password } = req.body
 
-  //  var sql = 'SELECT * FROM customers WHERE address = ' + mysql.escape(adr);
-    var sql = 'SELECT * FROM signup WHERE email_id = ' + mysql.escape(email_id) + 'and password= '+ mysql.escape(password) ;
-  //  var sql ='SELECT * FROM signup WHERE email_id = ? and password= ?';
-    
-    connection.query(sql,[email_id][password],(err, result) => {    
+    //  var sql = 'SELECT * FROM customers WHERE address = ' + mysql.escape(adr);
+    var sql = 'SELECT * FROM signup WHERE email_id = ' + mysql.escape(email_id) + 'and password= ' + mysql.escape(password);
+    //  var sql ='SELECT * FROM signup WHERE email_id = ? and password= ?';
+
+    connection.query(sql, [email_id][password], (err, result) => {
         if (err) {
             console.log('Errrrrrrrrrrrrrrrrrrrrrrrrrr', err)
             res.status(500).send(JSON.stringify({ error: err, status: 500 }))
-            
-    
-          }    
-
-
-       else
-       {
-                if(result.length>0)
-                {
-                    console.log("loign success");
-                    res.status(200).send({ data: req.body, status: 200 })
-                    
-                }
-                else
-                {
-                    res.status(204).send({ data: req.body, status: 204 })
-                    console.log("email and pass dont match");
-                    //res.status(204).send({ data: req.body, status: 204 })
-                }
+        }
+        else {
+            if (result.length > 0) {
+                console.log("loign success");
+                res.status(200).send({ data: req.body, status: 200 })
             }
+            else {
+                console.log("email and pass dont match");
+                res.status(500).send({ error: 'email and pass dont match', status: 204 })
+                //res.status(204).send({ data: req.body, status: 204 })
+            }
+        }
 
     });
 })
